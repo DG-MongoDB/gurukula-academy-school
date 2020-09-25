e95ece7e60395410df1ace2bbb3d30167ff39a36
DG-MongoDB <gundala.dilip@gmail.com>
2020-09-25 16:52:03 +0530

    Update 404.js
    changed  the file

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,7 @@ const NotFoundPage = () => (
   <div>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <p>Edited by Demo Team.</p>
-  </div>
+    </div>
 )
 
 export default NotFoundPage
